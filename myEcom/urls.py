b08b8d8e29e07af82c94f3b58646311aba3e7a5f
diksha1012 <trivedidiksha357@gmail.com>
2021-10-31 10:24:28 +0530

    change in urls

--- myEcom/urls.py
+++ myEcom/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',include('shop.urls')),
     path('index',include('shop.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     path('shop/base',include('shop.urls')),
    
